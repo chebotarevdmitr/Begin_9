e6e67ba4806f8c91fb1fc9475c69cd8140f4e6cd
DIM <4ebotarov_dim@inbox.ru>
2023-06-07 23:32:31 +0300

    Ternary operator

--- Begin_9 ter..cpp
+++ Begin_9 ter..cpp
@@ -11,11 +11,13 @@ int main()
    cout<<"   => Guest:  ";
 	cin>> guest_user_num;
 	
- if(host_user_num == guest_user_num)
+ /*
+  if(host_user_num == guest_user_num)
     cout<<" Correct! ";
 	else
 	cout<<" Failed! ";
-	
+ */
+    (host_user_num == guest_user_num)? cout<<" Correct! ":cout<<" Failed! ";// тернарный (тройричный) оператор	
 	
 	system("pause>0");
 }
